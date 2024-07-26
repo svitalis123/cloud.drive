28d92b9dfe020cdca1fc38056d6e3368a55641e3
svitalis123 <vitalis.wilson@yahoo.com>
2024-07-26 22:44:49 +0300

    interfere with tprovide

--- src/app/hooks/use-resources.ts
+++ src/app/hooks/use-resources.ts
@@ -1,5 +1,5 @@
 'use client'
-import { useQuery, useQueryClient } from '@tanstack/react-query'
+import { useQuery, useQueryClient, useMutation } from '@tanstack/react-query'
 import { ResourcesTypes } from '../types/types';
 
 interface MediaGalleryTypes {
@@ -19,12 +19,44 @@ export function useResources(options?: MediaGalleryTypes){
     initialData: options?.intialResources
   })
 
-  function addResources(results: Array<ResourcesTypes>){
-    queryClient.setQueryData(['resources', String(process.env.NEXT_PUBLIC_CLOUDINARY_TAG_NAME)], (old: Array<ResourcesTypes>) => {
-      return [...results, ...old]
-    } )
-    queryClient.invalidateQueries({queryKey:['resources', String(process.env.NEXT_PUBLIC_CLOUDINARY_TAG_NAME)]})
+  // function addResources(results: Array<ResourcesTypes>){
+  //   queryClient.setQueryData(['resources', String(process.env.NEXT_PUBLIC_CLOUDINARY_TAG_NAME)], (old: Array<ResourcesTypes>) => {
+  //     return [...results, ...old]
+  //   } )
+  //   queryClient.invalidateQueries({queryKey:['resources', String(process.env.NEXT_PUBLIC_CLOUDINARY_TAG_NAME)]})
+  // }
+
+  // Create a new mutation instance that we'll use to update our resources in the query state
+
+  const _addResources = useMutation({
+    mutationFn: async (resources: Array<ResourcesTypes>) => {
+      console.log(resources)
+      return resources
+    },
+    onMutate: async (newResources) => {
+      await queryClient.cancelQueries({ queryKey: ['resources'] })
+
+      const previousResources = queryClient.getQueryData(['resources'])
+
+      queryClient.setQueryData(['resources'], (old: Array<ResourcesTypes>) => [...newResources, ...old])
+
+      return { previousResources }
+    },
+    onError: (err, newResources, context) => {
+      queryClient.setQueryData(['resources'], context?.previousResources)
+    },
+    onSettled: () => {
+      queryClient.invalidateQueries({ queryKey: ['resources'] })
+    },
+  })
+
+  // Wrap the mutation function to a simpler API for application calls
+
+  function addResources(resources: Array<ResourcesTypes>) {
+    _addResources.mutate(resources);
   }
+
+
   return {
     resources,
     addResources
